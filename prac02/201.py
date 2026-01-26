0abfc6ef3419805ccd8c04c62e8da0d7b954a38b
Gulviii <g_abdigapbar>
2026-01-26 11:21:31 +0600

    add ed samples for the conditional topic

--- prac02/201.py
+++ prac02/201.py
@@ -1,5 +1,5 @@
-year=int(input())
-if year%4==0 or year%100==0:
-    print('yes')
+year = int(input())
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("YES")
 else:
-    print('no')
+    print("NO")
