db06d6776d000c916ad85e921727cead28df2025
Gulviii <g_abdigapbar>
2026-02-02 11:59:42 +0600

    add ed samples for the conditional topic

--- prac03/301.py
+++ prac03/301.py
@@ -1,6 +1,7 @@
-a=input()
-for i in range(a):
-    if a[i]%2==0:
-        print('valid')
-    else:
-        print('invalid')
+def is_valid_number(n: int) -> str:
+    for digit in str(n):
+        if int(digit) % 2 != 0:
+            return "Not valid"
+    return "Valid"
+n = int(input())
+print(is_valid_number(n))
